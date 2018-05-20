6416756e52b1689cf9442c0b9fe2279374373edf
KimotoYanke <kimotoyanke@gmail.com>
2018-05-20 14:43:36 +0900

    Add shebang

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,19 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import shebang from 'rollup-plugin-shebang'
 import pkg from './package.json'
 
 export default [
   {
     input: 'src/cli.js',
     external: ['ms'],
-    plugins: [babel({
-      exclude: 'node_modules/**'
-    })],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      shebang()
+    ],
     output: [
       { file: pkg.main, format: 'cjs' }
     ]
